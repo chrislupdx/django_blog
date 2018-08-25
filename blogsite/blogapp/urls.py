6a4b78b4149fe7b32e982cb6760bd8c597d1d418
chrislupdx <christopherlu.pdx@gmail.com>
2018-08-24 20:28:20 -0700

    added user login and logouts

--- blogsite/blogapp/urls.py
+++ blogsite/blogapp/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'blogapp' # for namespacing
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
 ]
 
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
