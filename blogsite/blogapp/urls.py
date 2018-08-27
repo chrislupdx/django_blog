a82c4eabd23b5c9d4cd34bbfc2cf202df915d4f6
chrislupdx <christopherlu.pdx@gmail.com>
2018-08-26 18:33:21 -0700

    started working over MV MVT connects for comment box

--- blogsite/blogapp/urls.py
+++ blogsite/blogapp/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'blogapp' # for namespacing
 urlpatterns = [
     path('', views.index, name='index'),
+	path('add_comment/', views.add_comment, name='add_comment'),
+
+#not sure what foreignkey we're passing there
 ]
 
 #Add Django site authentication urls (for login, logout, password management)
--- blogsite/blogapp/views.py
+++ blogsite/blogapp/views.py
@@ -11,8 +11,22 @@ def index(request):
 #we need to figure out where the incoming text is from if this is a pure "put thing is a pure post command
 #whelp we just made a fuckton of new links on this page
 @permission_required('comments.can_post')
-def add(request):
-    blog_text = request.POST['Blogpost'] #new link, we need to figure out if we should be sending it to the blog model)
-    blog_entry = Todo(text=todo_text) #link
-    blot_entry.save()
+
+def add_comment(request, commentid):
+	# comment_item = get_object_or_404(Comment, pk=commentid)
+    comment_entry = Comment(text= body, user= request.user)
+    comment_entry.save()
     return redirect(reverse('blogapp:index'))
+
+#so comment_entry is gonna be good for
+
+
+# do we have the views written to take comment text and send it to be back end?
+# do we have the veiws written to take the 
+
+# @permission_required('comments.can_post') #we need to figure out what arguments this would want to take
+# def add_blogpost(request):
+#     blog_text = request.POST['Blogpost'] #new link, we need to figure out if we should be sending it to the blog model)
+#     blog_entry = Blogpost(text=todo_text) #link
+#     blot_entry.save()
+#     return redirect(reverse('blogapp:index'))
